6181008994cab64f89c1b763541ec3faba777951
rydalund <54313809+rydalund@users.noreply.github.com>
2025-10-30 11:26:51 +0100

    Initial commit: Spring Boot booking system with Java 21

---
+++ src/main/java/ec/utb/bookingsystem/BookingSystemApplication.java
@@ -0,0 +1,13 @@
+package ec.utb.bookingsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookingSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookingSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ec/utb/bookingsystem/BookingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package ec.utb.bookingsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
