49f1bc19b9b07efa8da0c159afe6988bf294122d
rydalund <54313809+rydalund@users.noreply.github.com>
2025-11-03 10:39:12 +0100

    Add basic service

---
+++ src/main/java/ec/utb/bookingsystem/repository/BookingRepository.java
@@ -0,0 +1,9 @@
+package ec.utb.bookingsystem.repository;
+
+import ec.utb.bookingsystem.model.Booking;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BookingRepository extends JpaRepository<Booking, Long> {
+}
---
+++ src/main/java/ec/utb/bookingsystem/service/BookingService.java
@@ -0,0 +1,36 @@
+package ec.utb.bookingsystem.service;
+
+import ec.utb.bookingsystem.model.Booking;
+import ec.utb.bookingsystem.repository.BookingRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class BookingService {
+
+    private final BookingRepository bookingRepository;
+
+    @Autowired
+    public BookingService(BookingRepository bookingRepository) {
+        this.bookingRepository = bookingRepository;
+    }
+
+    public List<Booking> getAllBookings() {
+        return bookingRepository.findAll();
+    }
+
+    public Optional<Booking> getBookingById(Long id) {
+        return bookingRepository.findById(id);
+    }
+
+    public Booking saveBooking(Booking booking) {
+        return bookingRepository.save(booking);
+    }
+
+    public void deleteBooking(Long id) {
+        bookingRepository.deleteById(id);
+    }
+}
